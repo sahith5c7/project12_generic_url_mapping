3aaf3455b0de3384eec78c4051cfe6fdcb31edd8
sahithi5c7 <sahithipadmakar@gmail.com>
2023-11-20 22:59:09 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def dulqar(request):
+    return render(request,'dulqar.html')
+
+def sahithi(request):
+    return render(request,'sahithi.html')
